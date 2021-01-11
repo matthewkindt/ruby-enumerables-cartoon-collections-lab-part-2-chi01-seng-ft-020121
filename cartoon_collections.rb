ddd63799098b72caffcb611efc3c4c4f4af68f9e
Matthew Kindt <matthewkindt96@yahoo.com>
2021-01-11 16:57:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,7 @@ end
 planeteer_calls
 end
 planeteer_calls
+end
 
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
