e6dba872c53276c34382457c69b137ef4c294d48
Matthew Kindt <matthewkindt96@yahoo.com>
2021-01-11 17:44:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,6 @@ def summon_captain_planet(planeteer_calls)
 small_calls = planeteer_calls.map {|item| item.replace(["Earth!", "Wind!", "Fire!", "Water!", "Heart!"])
 end
 end
-end
 
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
