fc37e8463802cc04582d870e6cc8b11d6621fe78
Matthew Kindt <matthewkindt96@yahoo.com>
2021-01-11 17:47:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,9 +7,9 @@ end
 end
 
 def summon_captain_planet(planeteer_calls)
-small_calls = planeteer_calls.map {|item| item.replace(["Earth!", "Wind!", "Fire!", "Water!", "Heart!"])
-planeteer_calls.map! {|item| item.replace(["Earth!", "Wind!", "Fire!", "Water!", "Heart!"])
-end
+  
+  # Use an Enumerable to capitalize and add '!' to every element in the passed in array
+  # Return a new array of the results
 end
 
 def long_planeteer_calls(planeteer_calls)
