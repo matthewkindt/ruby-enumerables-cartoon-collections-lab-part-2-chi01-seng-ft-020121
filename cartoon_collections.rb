9825eb1bdefea9a0e9f90d61f3e1b62a1a4febc2
Matthew Kindt <matthewkindt96@yahoo.com>
2021-01-11 18:28:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,8 @@ end
 
 def summon_captain_planet(planeteer_calls)
   small_calls = []
-  planeteer_calls.
+  planeteer_calls.each do |element|
+    small_calls
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
 end
