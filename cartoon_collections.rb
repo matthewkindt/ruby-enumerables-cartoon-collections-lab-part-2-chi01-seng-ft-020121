74e9702c15c9d54124b87715e47ad449146a2911
Matthew Kindt <matthewkindt96@yahoo.com>
2021-01-11 18:26:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ end
 end
 
 def summon_captain_planet(planeteer_calls)
-  small_calls = planeteer_calls
-  planeteer_calls
+  small_calls = []
+  planeteer_calls.
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
 end
